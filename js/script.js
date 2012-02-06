4cdf3b389cc330a3991157580e25f3b50325b8db
Will Cosgrove <will@willcosgrove.com>
2012-02-06 17:16:38 -0600

    Fixed the mystery of the remote data not loading

--- js/script.js
+++ js/script.js
@@ -9,49 +9,15 @@ var template, feedData, renderedFeed;
 template = $("#feed-template").html();
 
 // Simulate call to remote server returning the feed as JSON data
-// $.get("/js/fake-feed.json", function(data){
-//   console.log(data);
-//   feedData = data;
-// })
-feedData = {
-  "items": [
-    {
-      "title": "News flash!  Rails is the best!",
-      "source": "http://iloverails.com",
-      "timestamp": "02-10-12",
-      "body": "Ruby on Rails is a breakthrough in lowering the barriers of entry to programming. Powerful web applications that formerly might have taken weeks or months to develop can be produced in a matter of days."
-    },
-    {
-      "title": "News flash!  Rails is the best!",
-      "source": "http://iloverails.com",
-      "timestamp": "02-10-12",
-      "body": "Ruby on Rails is a breakthrough in lowering the barriers of entry to programming. Powerful web applications that formerly might have taken weeks or months to develop can be produced in a matter of days."
-    },
-    {
-      "title": "News flash!  Rails is the best!",
-      "source": "http://iloverails.com",
-      "timestamp": "02-10-12",
-      "body": "Ruby on Rails is a breakthrough in lowering the barriers of entry to programming. Powerful web applications that formerly might have taken weeks or months to develop can be produced in a matter of days."
-    },
-    {
-      "title": "News flash!  Rails is the best!",
-      "source": "http://iloverails.com",
-      "timestamp": "02-10-12",
-      "body": "Ruby on Rails is a breakthrough in lowering the barriers of entry to programming. Powerful web applications that formerly might have taken weeks or months to develop can be produced in a matter of days."
-    },
-    {
-      "title": "News flash!  Rails is the best!",
-      "source": "http://iloverails.com",
-      "timestamp": "02-10-12",
-      "body": "Ruby on Rails is a breakthrough in lowering the barriers of entry to programming. Powerful web applications that formerly might have taken weeks or months to develop can be produced in a matter of days."
-    }
-  ]
+$.get("/js/fake-feed.json", function(data){
+  feedData = data;
+  renderFeed(); // Once data is received, render that feed!
+})
+
+function renderFeed(){
+  renderedFeed = Mustache.render(template, feedData);
+  $("#feed").html(renderedFeed);
 }
 
-renderedFeed = Mustache.render(template, feedData);
-
-console.log(renderedFeed);
-
-$("#feed").html(renderedFeed)
 
 
